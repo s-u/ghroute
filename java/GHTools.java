bcfd01e87240339d0ae78a414786485e1e7b5ecb
Simon Urbanek <inbox@urbanek.info>
2022-03-31 11:21:35 +1300

    Add getAllPaths

--- java/GHTools.java
+++ java/GHTools.java
@@ -2,12 +2,14 @@ package nz.urbanek.RGH;
 
 import com.graphhopper.util.*;
 import com.graphhopper.util.shapes.GHPoint;
+import com.graphhopper.ResponsePath;
+import com.graphhopper.GHResponse;
 
 public class GHTools {
     /* this is intentionally not static since even though
        it should be, because there is a problem using custom
        loaders from static classes */
-    double[] pointList2ll(com.graphhopper.util.PointList pl) {
+    public double[] pointList2ll(com.graphhopper.util.PointList pl) {
 	int n = pl.size();
 	double res[] = new double[n * 2];
 	int i = 0;
@@ -17,5 +19,9 @@ public class GHTools {
 	    i++;
 	}
 	return res;
+    }
+
+    public ResponsePath[] getAllPaths(GHResponse response) {
+	return response.getAll().toArray(new ResponsePath[0]);
     }
 }
